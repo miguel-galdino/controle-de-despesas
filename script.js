470c12dee2b89ff9824348dcd6cda2895d25b888
Miguel Lino <contatomiguellino@gmail.com>
2021-08-05 17:18:53 -0300

    first add

---
+++ script.js
@@ -0,0 +1,173 @@
+
+/* some refactor needed */
+
+const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const outcomeDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
+const form = document.querySelector('#form')
+const inputTransactionName = document.querySelector('#text')
+const inputTransactionAmount = document.querySelector('#amount')
+const setTwoNumberDecimal = event=> {
+    this.value = parseFloat(this.value).toFixed(2);
+}
+const inputTransactionDate = document.querySelector('#date')
+const changeOrderButton = document.querySelector('#organize-btn')
+
+
+
+const localStorageTransactions = JSON.parse(localStorage
+	.getItem('transactions'))
+
+let transactions = localStorage
+.getItem('transactions') !== null ? localStorageTransactions : []
+
+const removeTransaction = ID =>{
+	transactions = transactions
+	.filter(transaction => transaction.id !== ID )
+	
+	updateLocalStorage()
+	init()
+}
+
+
+
+const addTransactionIntoDOM = ({amount, name, id, date}) => {
+       
+       const operator = amount < 0 ? '-' : '+'
+       const CSSClass = amount < 0 ? 'minus' : 'plus'
+       const amountWithoutOperator = Math.abs(amount)
+       const li = document.createElement('li')
+
+ 
+
+       li.classList.add(CSSClass)
+       li.innerHTML = `
+       ${name} - ${date}
+       <span>${operator} R$ ${amountWithoutOperator}</span>
+       <button class="delete-btn" onClick="removeTransaction(${id})">
+       x</button>
+       `
+       if(changeOrderButton.innerHTML === `Mais Antigas`){
+       transactionsUl.append(li)
+
+       }else{
+         transactionsUl.prepend(li)
+       }
+
+}
+
+const changeOrder = () => {
+
+    if(changeOrderButton.innerHTML === `Mais Recentes`){
+	changeOrderButton.innerHTML = `Mais Antigas`
+      
+	   
+    }else{
+    	changeOrderButton.innerHTML = `Mais Recentes`
+    	
+    }
+
+    init()
+    
+}
+
+
+const getTotal = transactionsAmounts => 
+    transactionsAmounts
+	.reduce((accumulator, transaction) => accumulator + transaction, 0)
+	.toFixed(2)
+
+
+const getIncome = transactionsAmounts =>
+     transactionsAmounts
+	.filter(value => value > 0)
+	.reduce((accumulator, transaction) => accumulator + transaction, 0)
+	.toFixed(2)
+
+
+const getOutcome = transactionsAmounts =>
+	Math.abs(transactionsAmounts
+	.filter(value => value < 0)
+	.reduce((accumulator, transaction) => accumulator + transaction, 0))
+	.toFixed(2)
+
+
+const updateBalanceValues = () =>{
+	
+	const transactionsAmounts = transactions.map(({ amount }) => amount)
+	
+	const total = getTotal(transactionsAmounts)
+
+	const income = getIncome(transactionsAmounts)
+
+	const outcome = getOutcome(transactionsAmounts)
+
+	balanceDisplay.textContent = `R$ ${total}`
+	incomeDisplay.textContent = `R$ ${income}`
+	outcomeDisplay.textContent = `R$ ${outcome}`
+}
+
+
+const init = () =>{
+    
+    transactionsUl.innerHTML = ''
+	
+
+	transactions.forEach(addTransactionIntoDOM)
+	updateBalanceValues()
+}
+
+init()
+
+
+const updateLocalStorage = () =>{
+	localStorage.setItem('transactions', JSON.stringify(transactions))
+}
+
+const generateID = () => Math.round(Math.random() * 1000)
+
+
+const addToTransactionsArray = (transactionName, transactionAmount, transactionDate) =>{
+	transactions.push({
+   	id: generateID(), 
+   	name: transactionName,
+   	date: transactionDate,
+   	amount: Number(transactionAmount)
+   	
+   })
+}
+
+const clearInputs = () =>{
+   inputTransactionName.value = ''
+   inputTransactionAmount.value = ''
+   inputTransactionDate.value = ''
+}
+
+const handleFormSubmit = event =>{
+   event.preventDefault()
+
+   const transactionName = inputTransactionName.value.trim()
+   const transactionAmount = inputTransactionAmount.value.trim()
+   const transactionDate = inputTransactionDate.value.trim()
+   const isSomeInputEmpty = transactionName === '' || transactionAmount === '' || transactionDate === ''
+
+   if (isSomeInputEmpty){
+   	 alert("Por favor, preencha todos os campos")
+   	 return
+   }
+
+  
+   addToTransactionsArray(transactionName, transactionAmount, transactionDate)
+   init ()
+   updateLocalStorage()
+
+   clearInputs()
+
+}
+
+form.addEventListener('submit', handleFormSubmit);
+
+
+
+
